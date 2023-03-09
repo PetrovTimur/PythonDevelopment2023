06f3c420deaa6635e846b344537df8eecb28efa3
Petrov Timur <s02200459@gse.cs.msu.ru>
2023-03-09 18:44:36 +0300

    Add provided cow modes

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -9,10 +9,21 @@ parser.add_argument('-l', help='list all cowfiles on the current COWPATH', actio
 parser.add_argument('-n', help='turn off word-wrapping', action='store_true')
 parser.add_argument('-T', help='specify tongue string', type=str, default='')
 parser.add_argument('-W', help='width of the text bubble', type=int)
+parser.add_argument('-b', help='b option', action='store_true')
+parser.add_argument('-d', help='d option', action='store_true')
+parser.add_argument('-g', help='g option', action='store_true')
+parser.add_argument('-p', help='p option', action='store_true')
+parser.add_argument('-s', help='s option', action='store_true')
+parser.add_argument('-t', help='t option', action='store_true')
+parser.add_argument('-w', help='w option', action='store_true')
+parser.add_argument('-y', help='y option', action='store_true')
 
 args = parser.parse_args()
 
 if args.l:
     print(*cowsay.list_cows())
 else:
-    print(cowsay.cowsay(message=args.message, eyes=args.e, tongue=args.T, width=args.W, wrap_text=args.n, cowfile=args.f))
+    mask = 'bdgpstwy'
+    options = ''.join([c for c in mask if args.__dict__[c]])
+
+    print(cowsay.cowsay(message=args.message, eyes=args.e, tongue=args.T, width=args.W, wrap_text=args.n, cowfile=args.f, preset=options))
