e19d2719c17173bbe1b86aa2d21b0da0e3553f3a
Petrov Timur <s02200459@gse.cs.msu.ru>
2023-03-12 23:51:52 +0300

    Adds inform and gameplay functions

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,3 +1,6 @@
+from random import choice
+
+
 def bullscows(guess: str, secret: str) -> (int, int):
     bulls = sum(1 for i in range(len(guess)) if guess[i] == secret[i])
     cows = set(guess).intersection(set(secret)).__len__()
@@ -15,3 +18,20 @@ def ask(prompt: str, valid: list[str] = None) -> str:
         return word
     else:
         return input(prompt)
+
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
+
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    word = choice(words)
+    guess = ask("Введите слово: ", words)
+    tries = 0
+
+    while guess != word:
+        tries += 1
+        inform("Быки: {}, Коровы: {}", *bullscows(guess, word))
+        guess = ask("Введите слово: ", words)
+
+    return tries + 1
