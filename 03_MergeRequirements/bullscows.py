ebf0372bffecbeb72eaef1098f4870a4529441cd
Petrov Timur <s02200459@gse.cs.msu.ru>
2023-03-12 23:20:27 +0300

    Adds ask function

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -3,3 +3,15 @@ def bullscows(guess: str, secret: str) -> (int, int):
     cows = set(guess).intersection(set(secret)).__len__()
 
     return bulls, cows
+
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    if valid:
+        word = input(prompt)
+        while word not in valid:
+            print("Неизвестное слово, повторите попытку...")
+            word = input(prompt)
+
+        return word
+    else:
+        return input(prompt)
